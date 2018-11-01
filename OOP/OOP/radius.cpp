496c239094d07e0997b8a737e447597e8403ddca
Eswsx <957140834@qq.com>
2018-11-01 16:26:14 +0800

    Added create function array

--- OOP/OOP/radius.cpp
+++ OOP/OOP/radius.cpp
@@ -1,7 +1,7 @@
 #include"radius.h"
 #include"define.h"
 #include<iostream>
-
+//***********************************//
 double Circle::getRadius() {
 	return radius_;
 }
@@ -21,6 +21,8 @@ double Circle::getArea() {
 	return radius_ * radius_ * PI;
 
 }
+
+//*********************************************//
 
 void printCircle1(Circle c) {
 	std::cout << "The radius of the circle is "
@@ -42,7 +44,51 @@ void printCircle3(Circle* c) {
 		<< c -> getArea() << ".";
 	std::cout << std::endl;
 }
+//**************************************************//
+void setCircleArray(Circle* a, int size,double radius) {
+	for (int i = 0; i < size; i++) {
+		a[i].setRadius(i + radius);
+	}
+}
 
+void printCircleArray(Circle* a, int size) {
+	for (int i = 0; i < size; i++) {
+		std::cout << a[i].getRadius() << " ";
+	}
+}
+
+double getTotalArea(Circle* a, int size) {
+	double sum = 0;
+	for (int i = 0; i < size; i++) {
+		sum += a[i].getArea();
+	}
+	return sum;
+}
+
+void printFunctionCall() {
+	double radius;
+	std::cout << "Enter the radius of the circle :";
+	std::cin >> radius;
+	std::cout << std::endl;
+	Circle circle1(radius);
+	printCircle1(circle1);//值传递
+	printCircle2(circle1);//引用传递
+	printCircle3(&circle1);//指针传递
+}
+
+void printCreateAnArrayOfObjects(){
+	const int SIZE = 10;
+	double radius10;
+	Circle circleArray[SIZE];//创建对象数组
+	std::cout << "Enter the radius of the circle :";
+	std::cin >> radius10;
+	setCircleArray(circleArray, SIZE, radius10);
+	std::cout << "The radius of the circle has: ";
+	printCircleArray(circleArray, SIZE);
+	std::cout << std::endl;
+	std::cout << "The total area of these circles is:" << getTotalArea(circleArray, SIZE) << std::endl;
+
+}
 
 /*	
 	创建对象调用构造函数：有参,无参
--- OOP/OOP/radius.h
+++ OOP/OOP/radius.h
@@ -1,26 +1,23 @@
 #pragma once
 
-
-
 class Circle{
 	public:
-		
 		Circle();
-	
 		Circle(double newRedius);
-
 		double getArea();
-
 		double getRadius();
-
 		void setRadius(double radius);
 
-
 	private:
-
 		double radius_;
 };
-//********************************//
+
+void printFunctionCall();
 void printCircle1(Circle c);
-void printCircle2(Circle &c);
+void printCircle2(Circle& c);
 void printCircle3(Circle* c);
+
+void setCircleArray(Circle* a, int size,double radius);
+void printCircleArray(Circle* a, int size);
+double getTotalArea(Circle* a, int size);
+void printCreateAnArrayOfObjects();
